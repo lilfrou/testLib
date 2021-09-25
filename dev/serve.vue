dea873041df5902f5ecba98e77b931708adbafa2
lilfrou <b.boy-frou@hotmail.com>
2021-09-25 15:11:49 +0100

    first commit

---
+++ dev/serve.vue
@@ -0,0 +1,18 @@
+<script>
+import { defineComponent } from 'vue';
+// Uncomment import and local "components" registration if library is not registered globally.
+// import { LibfirasSample } from '@/entry.esm';
+
+export default defineComponent({
+  name: 'ServeDev',
+  // components: {
+  //  LibfirasSample,
+  // }
+});
+</script>
+
+<template>
+  <div id="app">
+    <libfiras-sample />
+  </div>
+</template>
---
+++ src/lib-components/index.js
@@ -0,0 +1,3 @@
+/* eslint-disable import/prefer-default-export */
+export { default as LibfirasSample } from './libfiras-sample.vue';
+export { default as test } from './test.vue';
