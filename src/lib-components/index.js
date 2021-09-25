653a8acb16761b93781a48e6431e0130c4e36af0
lilfrou <b.boy-frou@hotmail.com>
2021-09-25 21:37:17 +0100

    test

--- src/lib-components/index.js
+++ src/lib-components/index.js
@@ -1,3 +1,4 @@
 /* eslint-disable import/prefer-default-export */
 export { default as LibfirasSample } from './libfiras-sample.vue';
 export { default as test } from './test.vue';
+export { router } from '../router/index.js'
